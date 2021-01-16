bfc9e07880179acfdeaf536ba1eac78bfdf22f1e
André Mussio <almussio@gmail.com>
2021-01-16 11:39:49 -0300

    Inclusão inicial do projeto para estudo de CORS

---
+++ 10 RestWithSpringBootUdemy Scaffold CORS/src/main/java/br/com/andremussio/controllers/PersonController.java
@@ -0,0 +1,111 @@
+package br.com.andremussio.controllers;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.andremussio.data.vo.v1.PersonVO;
+import br.com.andremussio.services.PersonServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "Person endpoint", description = "Persons management", tags = {"endpoint /person"})
+@RestController
+@RequestMapping(value = "/api/person/v1")
+public class PersonController {
+	@Autowired
+	private PersonServices personServices;
+
+	/*
+	 * Method GET é default, não precisa ser informado.
+	 * MediaType.APPLICATION.JSON_VALUE é default, não precisa ser informado.
+	 * 
+	 * @GetMapping é uma abreviação para @RequestMapping do verbo GET.
+	 */
+	// @RequestMapping(method = RequestMethod.GET, produces =
+	// MediaType.APPLICATION_JSON_VALUE)
+	// Ao configurar a aplicação para usar mais de um tipo de serialização, o
+	// atributo "produces" passa a ser obrigatório!
+	@ApiOperation(value = "Lista todas as pessoas cadastradas")
+	@GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
+	public List<PersonVO> findAll() {
+		List<PersonVO> persons = personServices.findAll();		
+		//Adiciona links de auto-relacionamento para cada elemento da lista
+		persons
+			.stream()
+			.forEach(p -> p.add(
+					linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()
+				)
+			);
+		return persons;
+	}
+
+	// @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces =
+	// MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "Lista uma pessoa específica informada por {id}")
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
+		PersonVO personVO = personServices.findById(id);
+		// Adiciona um link de auto-relacionamento
+		personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return personVO;
+	}
+
+	/*
+	 * @PostMapping é uma abreviação para @RequestMapping do verbo POST.
+	 */
+
+	// @RequestMapping(method = RequestMethod.POST, consumes =
+	// MediaType.APPLICATION_JSON_VALUE, produces =
+	// MediaType.APPLICATION_JSON_VALUE)
+	// Método POST produz e consome JSON e XML
+	@ApiOperation(value = "Cadastra uma nova pessoa")
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
+	public PersonVO create(@RequestBody PersonVO person) {
+		PersonVO personVO = personServices.create(person);
+		// Adiciona um link de auto-relacionamento
+		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+		return personVO;
+	}
+
+	/*
+	 * @PutMapping é uma abreviação para @RequestMapping do verbo PUT.
+	 */
+	// @RequestMapping(method = RequestMethod.PUT, consumes =
+	// MediaType.APPLICATION_JSON_VALUE, produces =
+	// MediaType.APPLICATION_JSON_VALUE)
+	// Método PUT produz e consome JSON e XML
+	@ApiOperation(value = "Altera dados de uma pessoa já cadastrada e especificada por {id}")
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
+	public PersonVO update(@RequestBody PersonVO person) {
+		PersonVO personVO = personServices.update(person);
+		// Adiciona um link de auto-relacionamento
+		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+		return personVO;
+	}
+
+	/*
+	 * @DeleteMapping é uma abreviação para @RequestMapping do verbo DELETE.
+	 */
+	// @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@ApiOperation(value = "Remove uma pessoa já cadastrada e especificada por {id}")
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		personServices.delete(id);
+		return ResponseEntity.ok().build();
+	}
+}
---
+++ 10 RestWithSpringBootUdemy Scaffold CORS/src/main/java/br/com/andremussio/services/PersonServices.java
@@ -0,0 +1,98 @@
+package br.com.andremussio.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.andremussio.data.converter.DozerConverter;
+import br.com.andremussio.data.model.Person;
+import br.com.andremussio.data.vo.v1.PersonVO;
+import br.com.andremussio.exception.ResourceNotFoundException;
+import br.com.andremussio.repository.PersonRepository;
+
+/*
+ * @Service marca a classe informando ao Spring que ela deverá ser gerenciada de forma a 
+ * permitir que o Spring a injete automaticamente nos códigos que a utilizarem.
+ * Os códigos que necessitarem dessa classe, deverão recebê-la por injeção de dependência, através o uso
+ * da anotação @Autowired.
+ */
+@Service
+public class PersonServices {
+
+	@Autowired
+	PersonRepository repository;
+
+	public PersonVO create(PersonVO person) {
+		/*
+		 * A partir do Java 11, foi implementada a declaração de variáveis com inferência de tipo (var).
+		 * Com isso, não é necessário declarar a variável do tipo do objeto que ela vai receber.
+		 * Com o uso do "var", o Java automaticamente reconhece o tipo.
+		 * Exemplo de como ficaria com o "var":
+		 * 
+		var entity = DozerConverter.parseObject(person, Person.class);
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		*/
+		Person entity = DozerConverter.parseObject(person, Person.class);
+		PersonVO vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+
+	public PersonVO update(PersonVO person) {
+		Person entity = repository.findById(person.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		entity.setPayDay(person.getPayDay());
+
+		PersonVO vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+		repository.delete(entity);
+	}
+
+	public PersonVO findById(Long id) {
+		/*
+		 * Método orElseThrow dispara uma exceção caso não seja encontrado o objeto
+		 * informado no id. Será criada a exceção ResourceNotFoundException para tratar
+		 * este caso, de forma a não retornar uma exceção genérica.
+		 */
+
+		/*
+		 * Forma 1 de tratamento e retorno: Forma básica ResourceNotFoundException
+		 * resourceNotFoundException = new
+		 * ResourceNotFoundException("No records found for this ID"); Optional<PersonVO>
+		 * person = repository.findById(id); if(person.isEmpty()) throw
+		 * resourceNotFoundException; return person.get();
+		 */
+
+		/*
+		 * Forma 2 de tratamento e retorno: Com Supplier de exceção
+		 * ResourceNotFoundException exceptionSupplier = new
+		 * ResourceNotFoundException("No records found for this ID"); return
+		 * repository.findById(id).orElseThrow((Supplier<? extends
+		 * ResourceNotFoundException>) exceptionSupplier);
+		 */
+
+		/*
+		 * Forma 3 de tratamento e retorno: Com Lambda
+		 */
+		Person entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		PersonVO vo = DozerConverter.parseObject(entity, PersonVO.class);
+		return vo;
+	}
+
+	public List<PersonVO> findAll() {
+		List<PersonVO> listVO = DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
+		return listVO;
+	}
+
+}
